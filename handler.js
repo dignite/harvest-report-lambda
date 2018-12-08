714266ca24e74afa4f929c04d12bf2b4bdbc2c72
Daniel Ignat <daniel.ignat@urbit.com>
2018-12-08 22:28:49 +0100

    Introduce environment variables

--- handler.js
+++ handler.js
@@ -1,14 +1,14 @@
 'use strict';
 
+const accessToken = process.env.HARVEST_ACCESS_TOKEN;
+const accountId = process.env.HARVEST_ACCOUNT_ID;
+
 module.exports.hello = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
+      message: `${accessToken}, ${accountId}`,
       input: event,
     }),
   };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
 };
