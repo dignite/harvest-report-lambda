d0d3287c1c3f88bebdb3cc6780a69959dd18d8ec
Daniel Ignat <daniel.ignat@urbit.com>
2018-12-09 04:53:59 +0100

    Rename handler function

--- handler.js
+++ handler.js
@@ -3,7 +3,7 @@
 const accessToken = process.env.HARVEST_ACCESS_TOKEN;
 const accountId = process.env.HARVEST_ACCOUNT_ID;
 
-module.exports.hello = async (event, context) => {
+module.exports.hours = async (event, context) => {
   return {
     statusCode: 200,
     body: JSON.stringify({
