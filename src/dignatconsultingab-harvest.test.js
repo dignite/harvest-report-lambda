88be53369a86fdecf154851c09d84a08a561b995
Daniel Ignat <dignite@users.noreply.github.com>
2018-12-10 10:17:50 +0100

    Add jest

---
+++ src/dignatconsultingab-harvest.test.js
@@ -0,0 +1,6 @@
+const harvestClient = require("./dignatconsultingab-harvest");
+
+describe(harvestClient.getUnbilledRelevantTimeEntries, () => {
+  xtest("should return all unbilled billable hours", () => {});
+  xtest("should return non-billable hours from the current month", () => {});
+});
