99fdd78a03095b3bb96c2f804efc69d32a794f31
Daniel Ignat <dignite@users.noreply.github.com>
2018-12-12 04:55:12 +0100

    Test options used when creating authenticaed harvest

--- src/authenticated-harvest.js
+++ src/authenticated-harvest.js
@@ -1,10 +1,11 @@
 const Harvest = require("harvest").default;
+const { HARVEST_ACCESS_TOKEN, HARVEST_ACCOUNT_ID } = require("./process-env");
 const harvest = new Harvest({
   userAgent: "dignatconsultingab-harvest-report-lambda (daniel@dignat.se)",
   concurrency: 1,
   auth: {
-    accessToken: process.env.HARVEST_ACCESS_TOKEN,
-    accountId: process.env.HARVEST_ACCOUNT_ID
+    accessToken: HARVEST_ACCESS_TOKEN,
+    accountId: HARVEST_ACCOUNT_ID
   }
 });
 
--- src/authenticated-harvest.test.js
+++ src/authenticated-harvest.test.js
@@ -7,9 +7,34 @@ jest.mock("harvest", () => ({
   }
 }));
 
+jest.mock("./process-env");
+
 describe("authenticatedHarvest", () => {
   test("should return instance of harvest api wrapper", () => {
     expect(authenticatedHarvest).toBeInstanceOf(mockHarvest.default);
   });
-  xtest("should authenticate with account id and access token", () => {});
+  test("should authenticate with user agent", () => {
+    expect(authenticatedHarvest.options).toEqual(
+      expect.objectContaining({
+        userAgent: "dignatconsultingab-harvest-report-lambda (daniel@dignat.se)"
+      })
+    );
+  });
+  test("should authenticate with no concurrency", () => {
+    expect(authenticatedHarvest.options).toEqual(
+      expect.objectContaining({
+        concurrency: 1
+      })
+    );
+  });
+  test("should authenticate with account id and access token", () => {
+    expect(authenticatedHarvest.options).toEqual(
+      expect.objectContaining({
+        auth: {
+          accessToken: "Value from process.env.HARVEST_ACCESS_TOKEN",
+          accountId: "Value from process.env.HARVEST_ACCOUNT_ID"
+        }
+      })
+    );
+  });
 });
