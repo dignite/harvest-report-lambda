d662b9892eed722dd1eb3f648fd87dd1c39829fc
Daniel Ignat <dignite@users.noreply.github.com>
2018-12-19 10:54:26 +0100

    Add metadata to csv file

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const costSummary = require("./cost-summary");
 const { serialize } = require("./serializer");
 const { timestampForFilename } = require("./date");
 const serverlessAbsolutePath = require("./serverless-absolute-path");
+const json2csv = require("json2csv");
 
 module.exports.root = async () => {
   return {
@@ -20,34 +21,52 @@ module.exports.hours = async event => {
   return {
     statusCode: 200,
     body: serialize({
-      meta: {
-        description:
-          "*All* unbilled billable hours, and any non-billable hours logged for the current month.",
-        totalUnbilledHours: timeSummary.totalSum(relevantTimeEntries),
-        totalUnbilledHoursPerWeek: timeSummary.perWeek(relevantTimeEntries),
-        unbilledInvoice: costSummary.totalSum(relevantTimeEntries),
-        csvFile: serverlessAbsolutePath.resolve(event, `${event.path}.csv`)
-      },
+      meta: await hoursMeta(event),
       timeEntriesPerDay: timePerDay.merge(relevantTimeEntries)
     })
   };
 };
 
-module.exports.hoursCsv = async () => {
+module.exports.hoursCsv = async event => {
   const relevantTimeEntries = await timeEntries.getRelevantUnbilled();
   const timeEntriesPerDay = timePerDay.merge(relevantTimeEntries);
+  const meta = await hoursMeta(event);
+  const transposedMeta = objectToArrayOfArrays(meta);
   return {
     statusCode: 200,
     headers: {
       "Content-Disposition": `attachment; filename=unbilled-hours-${timestampForFilename()}.csv`
     },
-    body:
-      "\uFEFFDate;Task;Hours\n" +
-      timeEntriesPerDay
-        .map(
-          timeEntry =>
-            `${timeEntry.date};${timeEntry.name};${timeEntry.billableHours}`
-        )
-        .join("\n")
+    body: [
+      json2csv.parse(transposedMeta, {
+        withBOM: true,
+        delimiter: ";",
+        header: false
+      }),
+      json2csv.parse(timeEntriesPerDay, {
+        withBOM: false,
+        delimiter: ";",
+        fields: ["date", "name", "billableHours", "comment"]
+      })
+    ].join("\n\n")
+  };
+};
+
+const objectToArrayOfArrays = obj => {
+  if (typeof obj === "object") {
+    return Object.keys(obj).map(key => [key, obj[key]]);
+  }
+  return obj;
+};
+
+const hoursMeta = async event => {
+  const relevantTimeEntries = await timeEntries.getRelevantUnbilled();
+  return {
+    description:
+      "*All* unbilled billable hours, and any non-billable hours logged for the current month.",
+    totalUnbilledHours: timeSummary.totalSum(relevantTimeEntries),
+    totalUnbilledHoursPerWeek: timeSummary.perWeek(relevantTimeEntries),
+    unbilledInvoice: costSummary.totalSum(relevantTimeEntries),
+    csvFile: serverlessAbsolutePath.resolve(event, `${event.path}.csv`)
   };
 };
