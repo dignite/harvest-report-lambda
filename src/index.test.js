0afaac96632426b613185cfda8a0f3fb4a27a348
Daniel Ignat <dignite@users.noreply.github.com>
2018-12-26 04:05:08 +0100

    Test hours csv function

--- src/index.test.js
+++ src/index.test.js
@@ -3,6 +3,8 @@ const mockTimeEntries = require("./time-entries");
 const mockTimePerDay = require("./time-per-day");
 const mockMeta = require("./meta");
 const mockSerializer = require("./serializer");
+const mockDateModule = require("./date");
+const mockCsv = require("./npm-package-encapsulation/csv");
 
 jest.mock("./time-entries", () => ({
   getRelevantUnbilled: jest.fn()
@@ -14,6 +16,13 @@ jest.mock("./meta", () => ({
   hoursMeta: jest.fn()
 }));
 jest.mock("./serializer");
+jest.mock("./date", () => ({
+  timestampForFilename: jest.fn()
+}));
+jest.mock("./npm-package-encapsulation/csv", () => ({
+  csvFromObjectTransposed: jest.fn(),
+  csvFromObjectWithoutBOM: jest.fn()
+}));
 
 describe(functions.root, () => {
   test("should return status code not found", async () => {
@@ -69,6 +78,59 @@ describe(functions.hours, () => {
             timeEntriesPerDay: mockTimePerDay.merge(relevantTimeEntries)
           })
         )
+      })
+    );
+  });
+});
+
+describe(functions.hoursCsv, () => {
+  const event = {
+    path: "/my-path"
+  };
+
+  test("should return status code and attachment file name", async () => {
+    mockDateModule.timestampForFilename.mockReturnValue("2011-11-22");
+
+    const result = await functions.hoursCsv(event);
+
+    expect(result).toEqual(
+      expect.objectContaining({
+        headers: {
+          "Content-Disposition": `attachment; filename=unbilled-hours-2011-11-22.csv`
+        },
+        statusCode: 200
+      })
+    );
+  });
+
+  test("should return csv containing meta and per-day entries", async () => {
+    const relevantTimeEntries = ["fakeTimeEntry1", "fakeTimeEntry2"];
+    mockTimeEntries.getRelevantUnbilled.mockReturnValue(relevantTimeEntries);
+    mockMeta.hoursMeta.mockImplementation((timeEntries, event) => ({
+      "hoursMeta() of": {
+        timeEntries,
+        event
+      }
+    }));
+    mockTimePerDay.merge.mockImplementation(input => ({
+      "mockTimePerDay.merge() of": input
+    }));
+    mockCsv.csvFromObjectTransposed.mockImplementation(input => ({
+      "mockCsv.csvFromObjectTransposed() of": input
+    }));
+    mockCsv.csvFromObjectWithoutBOM.mockImplementation(input => ({
+      "mockCsv.csvFromObjectWithoutBOM() of": input
+    }));
+
+    const result = await functions.hoursCsv(event);
+
+    expect(result).toEqual(
+      expect.objectContaining({
+        body: `${mockCsv.csvFromObjectTransposed(
+          mockMeta.hoursMeta(relevantTimeEntries, event)
+        )}\n\n${mockCsv.csvFromObjectWithoutBOM(
+          mockTimePerDay.merge(relevantTimeEntries)
+        )}`
       })
     );
   });
