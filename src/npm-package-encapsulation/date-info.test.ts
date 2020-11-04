2082d288d5ec03c491b2fd0121ea108b0b2b5234
Daniel Edholm Ignat <daniel@dignat.se>
2020-11-04 19:07:59 +0100

    Convert date-info to typescript
    Fix types for date-info

---
+++ src/npm-package-encapsulation/date-info.test.ts
@@ -0,0 +1,10 @@
+import { getWeekNumber } from "./date-info";
+import mockGetISOWeek from "date-fns/getISOWeek";
+
+jest.mock("date-fns/getISOWeek");
+
+describe(getWeekNumber, () => {
+  test("should be alias for date-fns/getISOWeek", () => {
+    expect(getWeekNumber).toEqual(mockGetISOWeek);
+  });
+});
