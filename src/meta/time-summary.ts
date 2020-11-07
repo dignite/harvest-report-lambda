01ed6d2f5c709a6076dd042fbb4a2e0abe6970a5
Daniel Edholm Ignat <daniel@dignat.se>
2020-11-07 12:29:56 +0100

    Convert time-summary to typescript
    Fix types for time-summary

---
+++ src/meta/time-summary.ts
@@ -0,0 +1,34 @@
+import { getWeekNumber } from "../npm-package-encapsulation/date-info";
+import groupBy from "lodash.groupby";
+import mapValues from "lodash.mapvalues";
+import { HarvestReportLambdaTimeEntry } from "../time-entries";
+
+type BillableHoursOnTimeEntry = Pick<
+  HarvestReportLambdaTimeEntry,
+  "billableHours"
+>;
+
+export const totalSum = (timeEntries: BillableHoursOnTimeEntry[]): number =>
+  timeEntries.reduce(
+    (previous, timeEntry) =>
+      sumPreservingOneDecimal(previous, timeEntry.billableHours),
+    0
+  );
+
+type BillableHoursByWeek = Record<string, number>;
+
+export const perWeek = (
+  timeEntries: HarvestReportLambdaTimeEntry[]
+): BillableHoursByWeek => {
+  const hoursWithWeekNumber = timeEntries.map((timeEntry) => ({
+    billableHours: timeEntry.billableHours,
+    week: `w${getWeekNumber(Date.parse(timeEntry.date))}`,
+  }));
+
+  const hoursByWeek = groupBy(hoursWithWeekNumber, "week");
+
+  return mapValues(hoursByWeek, totalSum);
+};
+
+const sumPreservingOneDecimal = (value1: number, value2: number) =>
+  (value1 * 10 + value2 * 10) / 10;
