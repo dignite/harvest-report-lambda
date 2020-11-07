a3ca14e8651e0b5248e16c47d6e75796e359d734
Daniel Edholm Ignat <daniel@dignat.se>
2020-11-07 12:26:35 +0100

    Rename date to .ts
    Fix types for date

--- src/date.ts
+++ src/date.ts
@@ -1,6 +1,4 @@
-const startOfMonth = () => {
+export const startOfMonth = () => {
   const now = new Date();
   return new Date(Date.UTC(now.getFullYear(), now.getMonth(), 1));
 };
-
-module.exports.startOfMonth = startOfMonth;
