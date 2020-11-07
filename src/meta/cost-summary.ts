70e928e03d8a4a73dd17b3da008adfb6873aa238
Daniel Edholm Ignat <daniel@dignat.se>
2020-11-07 13:13:57 +0100

    Convert cost-summary to typescript
    Fix types for cost-summary

---
+++ src/meta/cost-summary.ts
@@ -0,0 +1,20 @@
+import { SEK } from "../npm-package-encapsulation/swedish-crowns";
+import { HarvestReportLambdaTimeEntry } from "../time-entries";
+
+interface CostSummary {
+  excludingVAT: string;
+  includingVAT: string;
+}
+
+export const totalSum = (
+  timeEntries: HarvestReportLambdaTimeEntry[]
+): CostSummary => {
+  const cost = timeEntries.reduce(
+    (previous, timeEntry) => previous.add(SEK(timeEntry.cost)),
+    SEK(0)
+  );
+  return {
+    excludingVAT: cost.toString(),
+    includingVAT: cost.addVAT(25).toString(),
+  };
+};
