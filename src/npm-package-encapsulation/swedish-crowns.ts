89f7395f75d61b4443651647fd4e62e2c74eb2f1
Daniel Edholm Ignat <daniel@dignat.se>
2020-11-08 19:48:16 +0100

    Rename swedish-crowns to .ts
    Fix types for swedish-crowns

--- src/npm-package-encapsulation/swedish-crowns.ts
+++ src/npm-package-encapsulation/swedish-crowns.ts
@@ -1,30 +1,31 @@
-const Dinero = require("dinero.js");
+import DineroFactory from "dinero.js";
+import Dinero from "dinero.js";
 
-const SEK = (amount) => encapsulateDineroAsSEK(dineroFromAmount(amount));
+export const SEK = (amount: number) =>
+  encapsulateDineroAsSEK(dineroFromAmount(amount));
 
-const dineroFromAmount = (amount) =>
+const dineroFromAmount = (amount: number) =>
   Dinero({
     amount: parseInt((amount * 100).toFixed(0), 10),
     currency: "SEK",
-    locale: "sv-SE",
   });
 
-const encapsulateDineroAsSEK = (dineroInstance) => ({
-  add: (sekToAdd) => {
+interface SEKInstance extends ReturnType<typeof encapsulateDineroAsSEK> {}
+
+const encapsulateDineroAsSEK = (dineroInstance: DineroFactory.Dinero) => ({
+  add: (sekToAdd: SEKInstance) => {
     const dinero1 = dineroFromAmount(sekToAdd.getAmount());
     const dinero2 = dineroInstance;
     return encapsulateDineroAsSEK(dinero1.add(dinero2));
   },
-  addVAT: (vat) => {
+  addVAT: (vat: number) => {
     const withVAT = dineroInstance.multiply((100 + vat) / 100);
     return encapsulateDineroAsSEK(withVAT);
   },
-  multiply: (multiplier) => {
+  multiply: (multiplier: number) => {
     const multiplied = dineroInstance.multiply(multiplier);
     return encapsulateDineroAsSEK(multiplied);
   },
   getAmount: () => dineroInstance.getAmount() / 100,
   toString: () => dineroInstance.toFormat("0.00 dollar"),
 });
-
-module.exports.SEK = SEK;
