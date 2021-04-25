19301169c605715fe28e02dfc3782d7eb89fb7a0
Daniel Edholm Ignat <daniel@dignat.se>
2021-04-25 19:54:56 +0200

    Remove unused event argument in integration test

--- src/index.integration.test.js
+++ src/index.integration.test.js
@@ -2,18 +2,7 @@ const functions = require("./");
 
 describe(`hours integration test with all real modules except those in the folder __mocks__`, () => {
   test("should return meta data for time entries and relevant time entries per day", async () => {
-    const event = {
-      path: "/my-path",
-      headers: {
-        "X-Forwarded-Proto": "https",
-        Host: "example.uri",
-      },
-      requestContext: {
-        stage: "integration-test",
-      },
-    };
-
-    const result = await functions.hours(event);
+    const result = await functions.hours();
 
     expect(result).toMatchSnapshot();
   });
