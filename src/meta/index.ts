ff6b8c82fdc6355ba35e680d62a94f873c360d25
Daniel Edholm Ignat <daniel@dignat.se>
2021-04-27 18:40:58 +0200

    Convert meta/index to typescript

---
+++ src/meta/index.ts
@@ -0,0 +1,22 @@
+import { totalSum, perWeek } from "./time-summary";
+import { totalSum as totalCostSum } from "./cost-summary";
+import { HarvestReportLambdaTimeEntry } from "../time-entries";
+
+interface Meta {
+  description: "*All* unbilled billable hours, and any non-billable hours logged for the current month.";
+  totalUnbilledHours: ReturnType<typeof totalSum>;
+  totalUnbilledHoursPerWeek: ReturnType<typeof perWeek>;
+  unbilledInvoice: ReturnType<typeof totalCostSum>;
+}
+
+export const hoursMeta = (
+  relevantTimeEntries: HarvestReportLambdaTimeEntry[]
+): Meta => {
+  return {
+    description:
+      "*All* unbilled billable hours, and any non-billable hours logged for the current month.",
+    totalUnbilledHours: totalSum(relevantTimeEntries),
+    totalUnbilledHoursPerWeek: perWeek(relevantTimeEntries),
+    unbilledInvoice: totalCostSum(relevantTimeEntries),
+  };
+};
