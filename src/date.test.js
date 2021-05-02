8271ad466a98b96f2f0e55e46cc43acee8856867
Daniel Edholm Ignat <daniel@dignat.se>
2021-05-02 13:48:03 +0200

    Make sure start of month is in UTC
    If so, we can use it as a 'from' filter against harvest.

--- src/date.test.js
+++ src/date.test.js
@@ -6,12 +6,12 @@ describe("date", () => {
     test("should find October 1st", () => {
       MockDate.set(new Date("2018-10-17"));
 
-      expect(date.startOfMonth()).toEqual(new Date("2018-10-01T00:00:00.000"));
+      expect(date.startOfMonth()).toEqual(new Date("2018-10-01T00:00:00.000Z"));
     });
     test("should find November 1st", () => {
       MockDate.set(new Date("2018-11-01"));
 
-      expect(date.startOfMonth()).toEqual(new Date("2018-11-01T00:00:00.000"));
+      expect(date.startOfMonth()).toEqual(new Date("2018-11-01T00:00:00.000Z"));
     });
   });
 
