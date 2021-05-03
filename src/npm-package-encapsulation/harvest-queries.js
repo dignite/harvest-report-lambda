97da69c1b4919013633f0b6462dfb954b927fdcd
Daniel Edholm Ignat <daniel@dignat.se>
2021-05-03 20:45:05 +0200

    Introduce harvest-queries

---
+++ src/npm-package-encapsulation/harvest-queries.js
@@ -0,0 +1,29 @@
+const harvest = require("./authenticated-harvest");
+
+module.exports.getUnbilledTimeEntries = async () => {
+  const timeEntries = await harvest.timeEntries.list({
+    is_billed: "false",
+  });
+  return timeEntries.time_entries.map((timeEntry) =>
+    timeEntry.notes
+      ? {
+          billable: timeEntry.billable,
+          billableRate: timeEntry.billable_rate || 0,
+          comment: timeEntry.notes,
+          date: timeEntry.spent_date,
+          hours: timeEntry.hours,
+          id: timeEntry.id,
+          isBilled: timeEntry.is_billed,
+          name: timeEntry.task.name,
+        }
+      : {
+          billable: timeEntry.billable,
+          billableRate: timeEntry.billable_rate || 0,
+          date: timeEntry.spent_date,
+          hours: timeEntry.hours,
+          id: timeEntry.id,
+          isBilled: timeEntry.is_billed,
+          name: timeEntry.task.name,
+        }
+  );
+};
