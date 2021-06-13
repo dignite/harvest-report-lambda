b765d8f5413482653f9fd7d71f0ce5635aed6203
Daniel Edholm Ignat <daniel@dignat.se>
2021-06-13 12:49:27 +0200

    Fix eslint violations for jest/require-top-level-describe
    Exception made for mock-service-worker (setup-tests.ts)

--- src/process-env.integration.test.ts
+++ src/process-env.integration.test.ts
@@ -1,11 +1,13 @@
 import { get } from "./process-env";
 
-test("should return undefined if environment variable not found", () => {
-  expect.assertions(1);
-  expect(get("WHlksadlkdsadasds")).toBeUndefined();
-});
+describe("procces-env wrapper", () => {
+  it("should return undefined if environment variable not found", () => {
+    expect.assertions(1);
+    expect(get("WHlksadlkdsadasds")).toBeUndefined();
+  });
 
-test("should return NODE_ENV test", () => {
-  expect.assertions(1);
-  expect(get("NODE_ENV")).toStrictEqual("test");
+  it("should return NODE_ENV test", () => {
+    expect.assertions(1);
+    expect(get("NODE_ENV")).toStrictEqual("test");
+  });
 });
