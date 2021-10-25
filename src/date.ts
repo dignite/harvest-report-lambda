507eb158b1512f6ba65e5b4d606f2d23dfc8bc4a
Daniel Edholm Ignat <daniel@dignat.se>
2021-10-25 19:46:42 +0200

    Show unbillable from last month too

--- src/date.ts
+++ src/date.ts
@@ -1,4 +1,4 @@
-export const startOfMonth = (): Date => {
+export const startOfLastMonth = (): Date => {
   const now = new Date();
-  return new Date(Date.UTC(now.getFullYear(), now.getMonth(), 1));
+  return new Date(Date.UTC(now.getFullYear(), now.getMonth() - 1, 1));
 };
