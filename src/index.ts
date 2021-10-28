2c8c16de6765911d960027ce1d59d66bcf3f6ede
Daniel Edholm Ignat <daniel@dignat.se>
2021-10-28 22:33:57 +0200

    Add 'frontend'

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,9 @@ import { serialize } from "./serializer";
 
 interface ServerlessLambdaResponse {
   statusCode: 404 | 200;
+  headers?: {
+    "Content-Type": "text/html";
+  };
   body: string;
 }
 
@@ -12,6 +15,32 @@ export const root = async (): Promise<ServerlessLambdaResponse> => {
   return {
     statusCode: 404,
     body: "Not Found",
+  };
+};
+
+export const frontend = async (): Promise<ServerlessLambdaResponse> => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Content-Type": "text/html",
+    },
+    body: `
+    <!DOCTYPE html>
+    <html lang="en">
+      <head>
+        <meta charset="utf-8">
+        <title>Hours</title>
+      </head>
+      <body>
+        <script>
+          const link = document.createElement("a");
+          link.href = location.href + "/api/hours";
+          link.innerText = "/api/hours";
+          document.body.appendChild(link);
+        </script>
+      </body>
+    </html>
+    `,
   };
 };
 
