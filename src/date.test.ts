dignite/dignatconsultingab-harvest-report-lambda

Fix eslint violations for jest/no-hooks
Exception made for mock-service-worker (setup-tests.ts)

src/date.test.ts
@@ -5,21 +5,27 @@ describe("date", () => {
   describe("startOfMonth function", () => {
     it("should find October 1st", () => {
       expect.assertions(1);
-      MockDate.set(new Date("2018-10-17"));
+      try {
+        MockDate.set(new Date("2018-10-17"));
 
-      expect(startOfMonth()).toStrictEqual(
-        new Date("2018-10-01T00:00:00.000Z")
-      );
+        expect(startOfMonth()).toStrictEqual(
+          new Date("2018-10-01T00:00:00.000Z")
+        );
+      } finally {
+        MockDate.reset();
+      }
     });
     it("should find November 1st", () => {
       expect.assertions(1);
-      MockDate.set(new Date("2018-11-01"));
+      try {
+        MockDate.set(new Date("2018-11-01"));
 
-      expect(startOfMonth()).toStrictEqual(
-        new Date("2018-11-01T00:00:00.000Z")
-      );
+        expect(startOfMonth()).toStrictEqual(
+          new Date("2018-11-01T00:00:00.000Z")
+        );
+      } finally {
+        MockDate.reset();
+      }
     });
   });
-
-  afterEach(MockDate.reset);
 });

dignite/harvest-report-lambda

src/date.test.ts
@@ -5,30 +5,40 @@ describe("date", () => {
   describe("startOfLastMonth function", () => {
     it("should find September 1st", () => {
       expect.assertions(1);
-      MockDate.set(new Date("2018-10-17"));
+      try {
+        MockDate.set(new Date("2018-10-17"));
 
-      expect(startOfLastMonth()).toStrictEqual(
-        new Date("2018-09-01T00:00:00.000Z")
-      );
+        expect(startOfLastMonth()).toStrictEqual(
+          new Date("2018-09-01T00:00:00.000Z")
+        );
+      } finally {
+        MockDate.reset();
+      }
     });
     it("should find October 1st", () => {
       expect.assertions(1);
-      MockDate.set(new Date("2018-11-01"));
+      try {
+        MockDate.set(new Date("2018-11-01"));
 
-      expect(startOfLastMonth()).toStrictEqual(
-        new Date("2018-10-01T00:00:00.000Z")
-      );
+        expect(startOfLastMonth()).toStrictEqual(
+          new Date("2018-10-01T00:00:00.000Z")
+        );
+      } finally {
+        MockDate.reset();
+      }
     });
 
     it("should find December 1st", () => {
       expect.assertions(1);
-      MockDate.set(new Date("2019-01-11"));
+      try {
+        MockDate.set(new Date("2019-01-11"));
 
-      expect(startOfLastMonth()).toStrictEqual(
-        new Date("2018-12-01T00:00:00.000Z")
-      );
+        expect(startOfLastMonth()).toStrictEqual(
+          new Date("2018-12-01T00:00:00.000Z")
+        );
+      } finally {
+        MockDate.reset();
+      }
     });
   });
-
-  afterEach(MockDate.reset);
 });