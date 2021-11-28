dignite/dignatconsultingab-harvest-report-lambda

Fix eslint violations for jest/valid-title

src/date.test.ts
@@ -2,7 +2,7 @@ import { startOfMonth } from "./date";
 import MockDate from "mockdate";
 
 describe("date", () => {
-  describe(startOfMonth, () => {
+  describe("startOfMonth function", () => {
     it("should find October 1st", () => {
       expect.assertions(1);
       MockDate.set(new Date("2018-10-17"));

dignite/harvest-report-lambda

src/date.test.ts
@@ -2,7 +2,7 @@ import { startOfLastMonth } from "./date";
 import MockDate from "mockdate";
 
 describe("date", () => {
-  describe(startOfLastMonth, () => {
+  describe("startOfLastMonth function", () => {
     it("should find September 1st", () => {
       expect.assertions(1);
       MockDate.set(new Date("2018-10-17"));