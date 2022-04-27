5d56a4e8bf8519cbe28a815974d8983ee6f8d898
Daniel Edholm Ignat <daniel@dignat.se>
2022-04-27 20:14:19 +0200

    Add startOfMonth and lastDayOfMonth

--- src/date.test.ts
+++ src/date.test.ts
@@ -1,4 +1,4 @@
-import { startOfLastMonth } from "./date";
+import { startOfLastMonth, startOfMonth, lastDayOfMonth } from "./date";
 import MockDate from "mockdate";
 
 describe("date", () => {
@@ -35,6 +35,86 @@ describe("date", () => {
 
         expect(startOfLastMonth()).toStrictEqual(
           new Date("2018-12-01T00:00:00.000Z")
+        );
+      } finally {
+        MockDate.reset();
+      }
+    });
+  });
+
+  describe("startOfMonth function", () => {
+    it("should find September 1st", () => {
+      expect.assertions(1);
+      try {
+        MockDate.set(new Date("2018-09-17"));
+
+        expect(startOfMonth()).toStrictEqual(
+          new Date("2018-09-01T00:00:00.000Z")
+        );
+      } finally {
+        MockDate.reset();
+      }
+    });
+    it("should find October 1st", () => {
+      expect.assertions(1);
+      try {
+        MockDate.set(new Date("2018-10-02"));
+
+        expect(startOfMonth()).toStrictEqual(
+          new Date("2018-10-01T00:00:00.000Z")
+        );
+      } finally {
+        MockDate.reset();
+      }
+    });
+
+    it("should find December 1st", () => {
+      expect.assertions(1);
+      try {
+        MockDate.set(new Date("2018-12-11"));
+
+        expect(startOfMonth()).toStrictEqual(
+          new Date("2018-12-01T00:00:00.000Z")
+        );
+      } finally {
+        MockDate.reset();
+      }
+    });
+  });
+
+  describe("lastDayOfMonth function", () => {
+    it("should find September 30th", () => {
+      expect.assertions(1);
+      try {
+        MockDate.set(new Date("2018-09-17"));
+
+        expect(lastDayOfMonth()).toStrictEqual(
+          new Date("2018-09-30T00:00:00.000Z")
+        );
+      } finally {
+        MockDate.reset();
+      }
+    });
+    it("should find October 31st", () => {
+      expect.assertions(1);
+      try {
+        MockDate.set(new Date("2018-10-02"));
+
+        expect(lastDayOfMonth()).toStrictEqual(
+          new Date("2018-10-31T00:00:00.000Z")
+        );
+      } finally {
+        MockDate.reset();
+      }
+    });
+
+    it("should find December 31st", () => {
+      expect.assertions(1);
+      try {
+        MockDate.set(new Date("2018-12-11"));
+
+        expect(lastDayOfMonth()).toStrictEqual(
+          new Date("2018-12-31T00:00:00.000Z")
         );
       } finally {
         MockDate.reset();
--- src/date.ts
+++ src/date.ts
@@ -2,3 +2,13 @@ export const startOfLastMonth = (): Date => {
   const now = new Date();
   return new Date(Date.UTC(now.getFullYear(), now.getMonth() - 1, 1));
 };
+
+export const startOfMonth = (): Date => {
+  const now = new Date();
+  return new Date(Date.UTC(now.getFullYear(), now.getMonth(), 1));
+};
+
+export const lastDayOfMonth = (): Date => {
+  const now = new Date();
+  return new Date(Date.UTC(now.getFullYear(), now.getMonth() + 1, 0));
+};
