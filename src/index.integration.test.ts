bf07ac92bc16ce26a51abc370da288bf1335dfd6
Daniel Edholm Ignat <daniel@dignat.se>
2022-05-01 21:12:42 +0200

    Split up handlers and rename some properties
    BREAKING CHANGE

--- src/index.integration.test.ts
+++ src/index.integration.test.ts
@@ -1,4 +1,4 @@
-import { hours } from "./";
+import { hours, hoursPerDay } from "./";
 import { server } from "./mock-service-worker/server";
 import { prepareGetTimeEntriesSuccess } from "./mock-service-worker/harvest-handlers";
 import { lastDayOfMonth, startOfMonth } from "./date";
@@ -13,7 +13,7 @@ jest.mock("./serializer", () => ({
 }));
 
 describe(`hours integration test mostly real modules`, () => {
-  it("should return meta data for time entries and relevant time entries per day", async () => {
+  it("should return meta data for time entries", async () => {
     expect.assertions(1);
     const from = startOfMonth();
     const formattedFromDate =
@@ -42,6 +42,41 @@ describe(`hours integration test mostly real modules`, () => {
     );
 
     const result = await hours();
+
+    expect(result).toMatchSnapshot();
+  });
+});
+
+describe(`hoursPerDay integration test mostly real modules`, () => {
+  it("should return relevant time entries per day", async () => {
+    expect.assertions(1);
+    const from = startOfMonth();
+    const formattedFromDate =
+      from.getFullYear() +
+      "-" +
+      ("0" + (from.getMonth() + 1)).slice(-2) +
+      "-" +
+      ("0" + from.getDate()).slice(-2);
+    const to = lastDayOfMonth();
+    const formattedToDate =
+      to.getFullYear() +
+      "-" +
+      ("0" + (to.getMonth() + 1)).slice(-2) +
+      "-" +
+      ("0" + to.getDate()).slice(-2);
+    server.resetHandlers(
+      prepareGetTimeEntriesSuccess({
+        userAgent:
+          "harvest-report-lambda (Value from process.env.USER_AGENT_EMAIL)",
+        accessToken: "Value from process.env.HARVEST_ACCESS_TOKEN",
+        accountId: "Value from process.env.HARVEST_ACCOUNT_ID",
+        isBilledQueryParameter: "false",
+        isFromQueryParameter: formattedFromDate,
+        isToQueryParameter: formattedToDate,
+      })
+    );
+
+    const result = await hoursPerDay();
 
     expect(result).toMatchSnapshot();
   });
