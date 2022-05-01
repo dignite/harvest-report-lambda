93435655eeab22cfad80fe5a6fe2f7be36294f5c
Daniel Edholm Ignat <daniel@dignat.se>
2022-05-01 20:43:33 +0200

    Remove unused startOfLastMonth function

--- src/date.ts
+++ src/date.ts
@@ -1,8 +1,3 @@
-export const startOfLastMonth = (): Date => {
-  const now = new Date();
-  return new Date(Date.UTC(now.getFullYear(), now.getMonth() - 1, 1));
-};
-
 export const startOfMonth = (): Date => {
   const now = new Date();
   return new Date(Date.UTC(now.getFullYear(), now.getMonth(), 1));
