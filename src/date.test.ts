b861cdc61e6dbb35195fd9192174f320b9b7bd51
Daniel Edholm Ignat <daniel@dignat.se>
2022-05-01 20:43:33 +0200

    Remove unused startOfLastMonth function

--- src/date.test.ts
+++ src/date.test.ts
@@ -1,47 +1,7 @@
-import { startOfLastMonth, startOfMonth, lastDayOfMonth } from "./date";
+import { startOfMonth, lastDayOfMonth } from "./date";
 import MockDate from "mockdate";
 
 describe("date", () => {
-  describe("startOfLastMonth function", () => {
-    it("should find September 1st", () => {
-      expect.assertions(1);
-      try {
-        MockDate.set(new Date("2018-10-17"));
-
-        expect(startOfLastMonth()).toStrictEqual(
-          new Date("2018-09-01T00:00:00.000Z")
-        );
-      } finally {
-        MockDate.reset();
-      }
-    });
-    it("should find October 1st", () => {
-      expect.assertions(1);
-      try {
-        MockDate.set(new Date("2018-11-01"));
-
-        expect(startOfLastMonth()).toStrictEqual(
-          new Date("2018-10-01T00:00:00.000Z")
-        );
-      } finally {
-        MockDate.reset();
-      }
-    });
-
-    it("should find December 1st", () => {
-      expect.assertions(1);
-      try {
-        MockDate.set(new Date("2019-01-11"));
-
-        expect(startOfLastMonth()).toStrictEqual(
-          new Date("2018-12-01T00:00:00.000Z")
-        );
-      } finally {
-        MockDate.reset();
-      }
-    });
-  });
-
   describe("startOfMonth function", () => {
     it("should find September 1st", () => {
       expect.assertions(1);
