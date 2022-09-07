79d3c7689d8fe2e75e1370953c9066be4052bcb6
Daniel Edholm Ignat <daniel.edholm.ignat@tradera.com>
2022-09-07 20:22:50 +0200

    Remove hours per day endpoint

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,11 +1,5 @@
-import {
-  root,
-  hours,
-  hoursPerDayForCurrentMonth,
-  invoiceForCurrentMonth,
-} from "./";
+import { root, hours, invoiceForCurrentMonth } from "./";
 import { get } from "./time-entries";
-import { merge } from "./time-per-day";
 import { getInvoiceSumExcludingVAT, hoursMetaSlim } from "./meta";
 import { serialize } from "./serializer";
 import { mocked } from "ts-jest/utils";
@@ -13,7 +7,6 @@ import { when } from "jest-when";
 import { startOfMonth, lastDayOfMonth } from "./date";
 
 jest.mock("./time-entries");
-jest.mock("./time-per-day");
 jest.mock("./meta");
 jest.mock("./serializer");
 jest.mock("./date", () => ({
@@ -88,65 +81,6 @@ describe("hours function", () => {
         endDate,
       },
     });
-
-    expect(result).toStrictEqual({
-      body: mockSerializedBody,
-      headers: {
-        "Access-Control-Allow-Credentials": true,
-        "Access-Control-Allow-Origin": "*",
-      },
-      statusCode: 200,
-    });
-  });
-});
-
-describe("hoursPerDayForCurrentMonth function", () => {
-  it("should return serialized meta data and time entries", async () => {
-    expect.assertions(1);
-    const relevantTimeEntries = [
-      {
-        billableHours: 4.1,
-        comment: "",
-        cost: 548.17,
-        date: "2018-11-04",
-        id: 1,
-        name: "Programming",
-      },
-      {
-        billableHours: 7.0,
-        comment: "",
-        cost: 935.9,
-        date: "2018-11-04",
-        id: 4,
-        name: "Programming",
-      },
-    ];
-    when(get)
-      .calledWith(startOfMonth(), lastDayOfMonth())
-      .mockResolvedValue(relevantTimeEntries);
-
-    const timeEntriesPerDay: ReturnType<typeof merge> = [
-      {
-        date: "2018-01-04",
-        name: "Programming",
-        billableHours: 11.1,
-        comment: "",
-      },
-    ];
-    when(mocked(merge))
-      .calledWith(relevantTimeEntries)
-      .mockReturnValue(timeEntriesPerDay);
-
-    const mockSerializedBody = `Serialized meta and time entries per day ${Date.now()}`;
-    when(mocked(serialize))
-      .calledWith(
-        expect.objectContaining({
-          timeEntriesPerDay: timeEntriesPerDay,
-        })
-      )
-      .mockReturnValue(mockSerializedBody);
-
-    const result = await hoursPerDayForCurrentMonth();
 
     expect(result).toStrictEqual({
       body: mockSerializedBody,
