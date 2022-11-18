09d4dd8cd51b0b1a7306636dd52369521a53a880
Daniel Edholm Ignat <daniel.edholm.ignat@tradera.com>
2022-11-18 20:38:16 +0100

    Add snapshot for invoice accepting startDate and endDate

--- src/index.integration.test.ts
+++ src/index.integration.test.ts
@@ -1,4 +1,4 @@
-import { hours, invoiceForCurrentMonth } from "./";
+import { hours, invoiceForCurrentMonth, invoice } from "./";
 import { server } from "./mock-service-worker/server";
 import { prepareGetTimeEntriesSuccess } from "./mock-service-worker/harvest-handlers";
 import { lastDayOfMonth, startOfMonth } from "./date";
@@ -70,6 +70,34 @@ describe(`invoiceForCurrentMonth integration test mostly real modules`, () => {
     );
 
     const result = await invoiceForCurrentMonth();
+
+    expect(result).toMatchSnapshot();
+  });
+});
+
+describe(`invoice integration test mostly real modules`, () => {
+  it("should return relevant invoice information", async () => {
+    expect.assertions(1);
+    const formattedFromDate = "2022-04-01";
+    const formattedToDate = "2022-04-30";
+    server.resetHandlers(
+      prepareGetTimeEntriesSuccess({
+        userAgent:
+          "harvest-report-lambda (Value from process.env.USER_AGENT_EMAIL)",
+        accessToken: "Value from process.env.HARVEST_ACCESS_TOKEN",
+        accountId: "Value from process.env.HARVEST_ACCOUNT_ID",
+        isBilledQueryParameter: "false",
+        isFromQueryParameter: formattedFromDate,
+        isToQueryParameter: formattedToDate,
+      })
+    );
+
+    const result = await invoice({
+      pathParameters: {
+        startDate: formattedFromDate,
+        endDate: formattedToDate,
+      },
+    });
 
     expect(result).toMatchSnapshot();
   });
