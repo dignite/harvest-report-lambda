3a1b043f4d7c0d5b71902869e177c5108b6dc5a5
Daniel Edholm Ignat <daniel.edholm.ignat@tradera.com>
2022-11-18 22:31:18 +0100

    Add byName route
    Unit tests missing, but we got snapshot

--- src/index.integration.test.ts
+++ src/index.integration.test.ts
@@ -1,4 +1,4 @@
-import { hours, invoice } from "./";
+import { hours, byName, invoice } from "./";
 import { server } from "./mock-service-worker/server";
 import { prepareGetTimeEntriesSuccess } from "./mock-service-worker/harvest-handlers";
 
@@ -30,6 +30,35 @@ describe(`hours integration test mostly real modules`, () => {
 
     const result = await hours({
       pathParameters: {
+        startDate: formattedFromDate,
+        endDate: formattedToDate,
+      },
+    });
+
+    expect(result).toMatchSnapshot();
+  });
+});
+
+describe(`byName integration test mostly real modules`, () => {
+  it("should return matching time entries", async () => {
+    expect.assertions(1);
+    const formattedFromDate = "2022-04-01";
+    const formattedToDate = "2022-04-30";
+    server.resetHandlers(
+      prepareGetTimeEntriesSuccess({
+        userAgent:
+          "harvest-report-lambda (Value from process.env.USER_AGENT_EMAIL)",
+        accessToken: "Value from process.env.HARVEST_ACCESS_TOKEN",
+        accountId: "Value from process.env.HARVEST_ACCOUNT_ID",
+        isBilledQueryParameter: "false",
+        isFromQueryParameter: formattedFromDate,
+        isToQueryParameter: formattedToDate,
+      })
+    );
+
+    const result = await byName({
+      pathParameters: {
+        name: "Programmering",
         startDate: formattedFromDate,
         endDate: formattedToDate,
       },
