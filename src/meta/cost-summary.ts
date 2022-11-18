cd2da7c33f22166b748ff32e53c2bdc6f15f963c
Daniel Edholm Ignat <daniel.edholm.ignat@tradera.com>
2022-11-18 20:18:41 +0100

    Remove totalSum

--- src/meta/cost-summary.ts
+++ src/meta/cost-summary.ts
@@ -1,24 +1,6 @@
 import { SEK } from "../npm-package-encapsulation/swedish-crowns";
 import { HarvestReportLambdaTimeEntry } from "../time-entries";
 
-interface CostSummary {
-  excludingVAT: string;
-  includingVAT: string;
-}
-
-export const totalSum = (
-  timeEntries: HarvestReportLambdaTimeEntry[]
-): CostSummary => {
-  const cost = timeEntries.reduce(
-    (previous, timeEntry) => previous.add(SEK(timeEntry.cost)),
-    SEK(0)
-  );
-  return {
-    excludingVAT: cost.toString(),
-    includingVAT: cost.addVAT(25).toString(),
-  };
-};
-
 export const totalExcludingVAT = (
   timeEntries: HarvestReportLambdaTimeEntry[]
 ): string => {
